5959e3ab0b8fd60ccd717dca3f060bb456118289
xxxJarelxxx <41522683+Jarelino@users.noreply.github.com>
2023-05-29 08:03:56 +0200

    s19

---
+++ 1. object-copy.js
@@ -0,0 +1,70 @@
+// DEEP COPY
+
+const objectCopy = (obj) => {
+    const cloneObj = {};
+
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            cloneObj[key] = obj[key];
+        }
+    }
+
+    return cloneObj;
+}
+
+const original = {
+    string: '1',
+    numb: 1,
+    object: {
+        asd: 'asd',
+        asd2: {
+            asd2: '123',
+        }
+    },
+    func: function () {
+        return 1;
+    }
+}
+
+const copy = objectCopy(original);
+
+original.string = '123'
+original.numb = 123
+original.object = {
+    asd: 'qwerty',
+}
+original.func = function () {
+    return 2;
+}
+
+console.log('Deep copy')
+console.log(copy.string);
+console.log(copy.numb);
+console.log(copy.object);
+console.log(copy.func());
+console.log('\n');
+
+// SELECTION COPY
+
+const ruleKeys = ['string', 'numb', 'object'];
+
+const copyRule = (key) => {
+    return ruleKeys.includes(key);
+}
+
+const selectionCopyFunc = (obj, rule) => {
+    const cloneObj = {};
+
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key) && rule(key)) {
+            cloneObj[key] = obj[key];
+        }
+    }
+
+    return cloneObj;
+}
+
+
+console.log('Selection copy')
+console.log(selectionCopyFunc(original, copyRule));
+console.log('\n');
--- s14/app.js
+++ s14/app.js
@@ -3,23 +3,44 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://romayaruk:sXoSxt6Vu1bHDEVr@cluster0.hu13b6c.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'asd',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
 
 app.use((req, res, next) => {
-  User.findById('647418f8412c3525801a919a')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -29,13 +50,12 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://romayaruk:sXoSxt6Vu1bHDEVr@cluster0.hu13b6c.mongodb.net/shop'
-  )
+  .connect(MONGODB_URI)
   .then(result => {
     User.findOne().then(user => {
       if (!user) {
